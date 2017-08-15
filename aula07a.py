86da40f089f7cc75fcaf37781c9d7e2c97bf2120
0T3B <roberto@h3b.com.br>
2017-08-15 01:15:00 -0300

    Aula 08

--- aula07a.py
+++ aula07a.py
@@ -8,7 +8,8 @@ print("Olá {:^15}!".format(nome))
 print("Olá {:*^20}!".format(nome))
 """
 
-"""n1=int(input("Digite um numero: "))
+"""
+n1=int(input("Digite um numero: "))
 n2=int(input("Digite outro numero: "))
 print("A soma é {1}, o produto é {2} e a divisao é {0:.2f}.".format((n1/n2),(n1+n2),(n1*n2)), end='')
 print("Divisão inteira {} e potência {}".format((n1//n2),((n1**n2))), end='')
---
+++ aula08a.py
@@ -0,0 +1,19 @@
+"""
+Aula08 - Utilizando módulos
+
+Exemplos de importação:
+import math --> importa o módulo math
+from math import ceil
+from math import ceil,floor
+
+# ceil - aredonda para cima
+# floor - arredonda para baixo
+# trunc -
+# pow - Exponenciação (elevado a)
+# sqrt - Calcula a raiz quadrada
+# factorial - Calcula número fatorial
+
+"""
+
+from math import ceil, floor
+
