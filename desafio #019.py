2f14623c88cc04ebbe88bc188d90f451a1717af5
0T3B <roberto@h3b.com.br>
2017-08-18 10:39:58 -0300

    Improvements from Resolution Video

--- desafio #019.py
+++ desafio #019.py
@@ -4,9 +4,9 @@ Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça 
 programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
 """
 
-from random import choice,sample
+from random import choice
 
 alunos=["Lorena","Daysa","Paty","Beto"]
-nome=(str(sample(alunos,1)))
+nome=choice(alunos)
 print("O aluno escolhido para apagar o quadro é: {}.".format(nome))
 
