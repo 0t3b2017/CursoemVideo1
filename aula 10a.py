c806dbfb57ddcab4f8165c3430d9cbf6bd38921e
0T3B <roberto@h3b.com.br>
2017-08-24 01:13:32 -0300

    21-Aug-2017

---
+++ aula 10a.py
@@ -0,0 +1,22 @@
+"""nome = str(input("Digite o seu nome: "))
+if nome == "Roberto":
+    print("Que nome bonito você tem!")
+else:
+    print("Seu nome é comum :(")
+print("Boa noite {}".format(nome))
+"""
+
+#"""
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2) / 2
+# Estrutura condicional
+#if m >= 6.0:
+#    print("Sua média foi boa! PARABENS")
+#else:
+#    print("Sua média foi ruim! ESTUDE MAIS")
+print("Sua média foi {:.2f}".format(m))
+# Estrutura condicional simples
+print("Sua média foi boa! PARABENS" if m >=6 else "Sua média foi ruim! ESTUDE MAIS")
+#"""
+
