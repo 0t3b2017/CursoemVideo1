0fb8668d1230f1f15f559cb0c598019e3b994678
0T3B <roberto@h3b.com.br>
2017-08-31 12:34:25 -0300

    31-Aug-2017

--- aula 10a.py
+++ aula 10a.py
@@ -10,13 +10,17 @@ print("Boa noite {}".format(nome))
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 m = (n1 + n2) / 2
-# Estrutura condicional
+
+#### Estrutura condicional
+## Só if -> condicional simples
+## com else -> condicional composta
+
 #if m >= 6.0:
 #    print("Sua média foi boa! PARABENS")
 #else:
 #    print("Sua média foi ruim! ESTUDE MAIS")
 print("Sua média foi {:.2f}".format(m))
-# Estrutura condicional simples
+# Estrutura condicional simplificada.
 print("Sua média foi boa! PARABENS" if m >=6 else "Sua média foi ruim! ESTUDE MAIS")
 #"""
 
